ad0e1bbc0751b7278060a1985ce8482352df5dab
quader-arian <arianq@my.yorku.ca>
2023-03-05 21:13:40 -0500

    welp still need to work on it

--- game/Assets/Scripts/DrawScript.cs
+++ game/Assets/Scripts/DrawScript.cs
@@ -6,7 +6,10 @@ public class DrawScript : MonoBehaviour
 {
     private Camera cam;
     public GameObject brush;
+    public GameObject sensor;
+    public GameObject allLines;
     private Rigidbody2D body;
+    private Vector3[] allPoints;
     LineRenderer currentLineRenderer;
 
     Vector2 lastPos;
@@ -14,17 +17,11 @@ public class DrawScript : MonoBehaviour
     void Start(){
         cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         body = GetComponent<Rigidbody2D>();
+        CreateBrush();
     }
 
     void Update(){
-        Draw();
-    }
-    
-    void Draw(){
-        if(Input.GetButtonDown("Fire1")){
-            CreateBrush();
-        }
-        else if(body.velocity != Vector2.zero){
+        if(body.velocity != Vector2.zero){
             Vector2 mousePos = this.transform.position;
             if(mousePos != lastPos){
                 AddAPoint(mousePos);
@@ -32,24 +29,35 @@ public class DrawScript : MonoBehaviour
             }
         }
         else{
-            currentLineRenderer = null;
+            if(checkIntersect(currentLineRenderer)){
+                currentLineRenderer = null;
+                //CreateBrush();
+            }
         }
-
     }
 
     void CreateBrush(){
         GameObject brushInstance = Instantiate(brush);
         currentLineRenderer = brushInstance.GetComponent<LineRenderer>();
+        brushInstance.transform.parent = allLines.transform;
 
         Vector2 mousePos = this.transform.position;;
 
         currentLineRenderer.SetPosition(0, mousePos);
         currentLineRenderer.SetPosition(1, mousePos);
+        //GameObject sensorInstance = Instantiate(sensor, mousePos, Quaternion.identity);
+        //sensorInstance.transform.parent = allLines.transform;
     }
 
     void AddAPoint(Vector2 pointPos){
         currentLineRenderer.positionCount++;
         int positionIndex = currentLineRenderer.positionCount -1;
         currentLineRenderer.SetPosition(positionIndex, pointPos);
+        //GameObject sensorInstance = Instantiate(sensor, pointPos, Quaternion.identity);
+        //sensorInstance.transform.parent = allLines.transform;
+    }
+
+    bool checkIntersect(LineRenderer line){
+        return false;
     }
 }
--- game/Assets/Scripts/GravityScript.cs
+++ game/Assets/Scripts/GravityScript.cs
@@ -8,16 +8,28 @@ public class GravityScript : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D col)
     {
+        GravityChange(col);
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        GravityChange(col);
+    }
+
+    void GravityChange(Collision2D col){
         if (col.gameObject.tag == "Up"){
             Physics2D.gravity = new Vector2(0, 9.8f);
             signal.transform.rotation = Quaternion.Euler(0, 0, 0);
-        }else if(col.gameObject.tag == "Left"){
+        }
+        if(col.gameObject.tag == "Left"){
             Physics2D.gravity = new Vector2(-9.8f, 0);
             signal.transform.rotation = Quaternion.Euler(0, 0, 90);
-        }else if(col.gameObject.tag == "Right"){
+        }
+        if(col.gameObject.tag == "Right"){
             Physics2D.gravity = new Vector2(9.8f, 0);
             signal.transform.rotation = Quaternion.Euler(0, 0, 270);
-        }else if(col.gameObject.tag == "Down"){
+        }
+        if(col.gameObject.tag == "Down"){
             Physics2D.gravity = new Vector2(0, -9.8f);
             signal.transform.rotation = Quaternion.Euler(0, 0, 180);
         }
