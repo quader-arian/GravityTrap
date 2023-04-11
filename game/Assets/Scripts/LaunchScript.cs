bb29693492d3220005fc4931dba304f9889b57d6
quader-arian <arianq@my.yorku.ca>
2023-04-11 18:11:27 -0400

    accidentally lost room 3 hold up

--- game/Assets/Scripts/LaunchScript.cs
+++ game/Assets/Scripts/LaunchScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LaunchScript : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class LaunchScript : MonoBehaviour
     private Camera cam;
     private Vector2 mousePos;
     private AudioSource source;
+    private bool canLaunch = true;
+    public GameObject dialogueBox;
+    public int dialogueCount;
+    public TMP_Text t;
 
     public AudioClip [] sounds;
 
@@ -26,7 +31,25 @@ public class LaunchScript : MonoBehaviour
     void Update()
     {
        if(Input.GetButtonDown("Fire1")){
-            Lauch();
+            if(canLaunch){
+                Lauch();
+            }else{
+                if(dialogueCount == 0){
+                    t.text = "THEY BUSTED ME UP PRETTY BAD.";
+                }else if(dialogueCount == 1){
+                    t.text = "HOW AM I SUPPOSED TO PROTECT EVERYONE NOW?";
+                }else if(dialogueCount == 2){
+                    t.text = "I SEE YOUR GRAVITY CHIP STILL WORKS.";
+                }else if(dialogueCount == 3){
+                    t.text = "WATCH OUT FOR THE ENEMY TO THE RIGHT.";
+                }else if(dialogueCount == 4){
+                    t.text = "TO TAKE EM OUT, MOVE AROUND AND CLOSE YOUR TRACE BY TOUCHING THE LIGHTNING FLUX POINTS ON YOUR TRAIL";
+                }else{
+                    t.text = "TRAPPING THEM WITHIN YOUR GRAVITY AURA SHOULD DO THE TRICK";
+                    canLaunch = true;
+                }
+                dialogueCount++;
+            }
             int num = Random.Range(0, sounds.Length);
             source.clip = sounds[num];
             source.Play();
@@ -41,5 +64,13 @@ public class LaunchScript : MonoBehaviour
         //float rotZ = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
         //transform.rotation = Quaternion.Euler(0, 0, rotZ);
         body.AddForce(lookDir.normalized * force, ForceMode2D.Impulse);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Dialogue" && dialogueCount <= 0){
+            canLaunch = false;
+            dialogueBox.SetActive(true);
+        }
     }
 }
---
+++ game/Assets/Scripts/SceneManagerScript1.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript1 : MonoBehaviour
+{
+    public string sceneName;
+    public string defaultGravity = "Up";
+    public float timer = 2f;
+    public GameObject t1;
+    public GameObject t2;
+
+    void Start(){
+        if (defaultGravity == "Up"){
+            Physics2D.gravity = new Vector2(0, 9.8f);
+        }
+        if(defaultGravity == "Left"){
+            Physics2D.gravity = new Vector2(-9.8f, 0);
+        }
+        if(defaultGravity == "Right"){
+            Physics2D.gravity = new Vector2(9.8f, 0);
+        }
+        if(defaultGravity == "Down"){
+            Physics2D.gravity = new Vector2(0, -9.8f);
+        }
+    }
+
+    void Update(){
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("EnemyComplete");
+        if(enemies.Length <= 0){
+            timer -= Time.deltaTime;
+            t1.SetActive(false);
+            t2.SetActive(true);
+            GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().isKinematic = false;
+            if(timer < 0){
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        
+    }
+}
